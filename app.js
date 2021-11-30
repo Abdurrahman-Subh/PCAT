2d8a1b0eb122e372763bad5a72c3986b8bd7e63a
Abdulrahman797 <70597324+Abdulrahman797@users.noreply.github.com>
2021-12-01 02:36:54 +0300

    Added adding photos function

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Photo = require('./models/Photos');
+
+const { urlencoded } = require('express');
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//ROUTING
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
 
+  res.render('index', {
+    photos,
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
