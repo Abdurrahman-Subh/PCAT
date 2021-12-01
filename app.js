39cbeb6547f9b0823ee6a818262b4ee0b8e8bb37
Abdulrahman797 <70597324+Abdulrahman797@users.noreply.github.com>
2021-12-02 01:01:53 +0300

    Added CRUD

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const ejs = require('ejs');
-const path = require('path');
 const mongoose = require('mongoose');
-
-const Photo = require('./models/Photos');
-
-const { urlencoded } = require('express');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
 const app = express();
 
 //Connect DB
@@ -18,24 +17,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//ROUTING
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-  res.render('index', {
-    photos,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
-});
+//ROUTING
+app.get('/', photoController.getAllPhotos); //Show All Photos
+app.get('/photos/:id', photoController.getPhoto); // Show Photo Page
+app.post('/photos', photoController.createPhoto); //Create New Photo
+app.put('/photos/:id', photoController.updatePhoto); //Update Photo
+app.delete('/photos/:id', photoController.deletePhoto); //Delete Photo
+
+app.get('/about', pageController.getAboutPage); //Get About Page
+app.get('/add', pageController.getAddPage); //Get Add Page
+app.get('/photos/edit/:id', pageController.getEditPage);
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
